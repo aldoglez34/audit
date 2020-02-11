27dd8eb0187ab1a11aeac1ae35489292e68d4aab
Aldo Solano González <aldoglez34@gmail.com>
2020-02-11 15:04:04 -0600

    msg

--- routes/api/balanzaRoutes.js
+++ routes/api/balanzaRoutes.js
@@ -70,7 +70,56 @@ router.post("/upload", function(req, res) {
         })
           .then(data => {
             // send report to the front end
-            // console.log(data);
+            let report = data.reduce((acc, obj) => {
+              let total_si = Math.round(
+                Number(
+                  parseFloat(obj.total_si)
+                    .toLocaleString()
+                    .replace(/,/g, "")
+                )
+              );
+              let total_c = Math.round(
+                Number(
+                  parseFloat(obj.total_c)
+                    .toLocaleString()
+                    .replace(/,/g, "")
+                )
+              );
+              let total_a = Math.round(
+                Number(
+                  parseFloat(obj.total_a)
+                    .toLocaleString()
+                    .replace(/,/g, "")
+                )
+              );
+              let total_sf = Math.round(
+                Number(
+                  parseFloat(obj.total_sf)
+                    .toLocaleString()
+                    .replace(/,/g, "")
+                )
+              );
+
+              let isSIZero = total_si === 0 ? true : false;
+              let isSFZero = total_sf === 0 ? true : false;
+              let diff_CyA = total_c - total_a;
+              let isCandATheSame = total_sf === 0 ? true : false;
+
+              let month = {
+                month: obj.month,
+                total_si,
+                total_c,
+                total_a,
+                total_sf,
+                isSIZero,
+                isSFZero,
+                isCandATheSame,
+                diff_CyA
+              };
+              acc.push(month);
+              return acc;
+            }, []);
+            console.log(report);
             res.json(data);
           })
           .then(() => {
